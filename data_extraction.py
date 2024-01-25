6d4fc7f1b7e4d16340a71c075e327822db804ff0
Nev Mellors <nevmellors@hotmail.com>
2024-01-25 10:16:43 +0000

    updated to include PDF reader

--- data_extraction.py
+++ data_extraction.py
@@ -1,4 +1,5 @@
 import database_utils #import from a foreign file
+from tabula import read_pdf
 db_conn = database_utils.DatabaseConnector() #global variable for script, saves a bit of typing every time
 import pandas as pd
 
@@ -7,6 +8,7 @@ class DataExtractor:
     """extracting database detail from the link in database_utils"""
     def __init__(self):
        self.users = self.read_rds_table()
+       self.card_util = self.retrieve_pdf_data()
        pass
 
     def read_rds_table(self):
@@ -14,6 +16,25 @@ class DataExtractor:
         read_rds_table_df = pd.read_sql_query('SELECT * FROM legacy_users',db_conn.engine)
 
         return read_rds_table_df
+    
+    def retrieve_pdf_data(self):
+        """Process to retrieve data stored in a PDF
+        TABULA add on for Pyton (see top of file)
+        IMPORT the read-pdf function
+        
+        """
+
+        pdf_link = 'https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf'#held as a variable so that can be changed easily at a later date.
+        card_data = read_pdf(pdf_link,pages= 'all') #pages= 'all' or only the first page is imported to dataframe
+        card_data_df = pd.concat(card_data)   #each page of PDF is a seperate list, so needs to be concatenated
+
+        return card_data_df
+
+
+
+        
+
         pass
 
+test = DataExtractor().card_util()
 
