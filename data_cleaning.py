9245e122774ce6495e7077355266be9608bc12a2
Nev Mellors <nevmellors@hotmail.com>
2024-01-25 11:43:04 +0000

    refactored some of the code to remove self where not needed

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,20 +1,18 @@
 import pandas as pd
 import numpy as np 
-import re
 import data_extraction
-df_for_clean = data_extraction.DataExtractor().users
-card_df = data_extraction.DataExtractor().card_util
 import database_utils
 out_db = database_utils.DatabaseConnector().out_engine
 
 
 class DataCleaning:
-    def __init__(self):
-       #self.db = self.clean_user_data()
-       #self.clean_card = self.clean_card_data()
+    def __init__():
        pass
-    def clean_user_data(self):
-     print(df_for_clean)
+    
+
+    def clean_user_data():
+     df_for_clean = data_extraction.DataExtractor.read_rds_table()
+    
 
      regex_expression = '^(?:(?:\(?(?:0(?:0|11)\)?[\s-]?\(?|\+)44\)?[\s-]?(?:\(?0\)?[\s-]?)?)|(?:\(?0))(?:(?:\d{5}\)?[\s-]?\d{4,5})|(?:\d{4}\)?[\s-]?(?:\d{5}|\d{3}[\s-]?\d{3}))|(?:\d{3}\)?[\s-]?\d{3}[\s-]?\d{3,4})|(?:\d{2}\)?[\s-]?\d{4}[\s-]?\d{4}))(?:[\s-]?(?:x|ext\.?|\#)\d{3,4})?$' #Our regular expression to match
      df_for_clean.loc[~df_for_clean['phone_number'].str.match(regex_expression), 'phone_number'] = np.nan
@@ -37,11 +35,10 @@ class DataCleaning:
      
      df_for_clean.to_sql('dim_users', out_db, if_exists='replace')
 
-     print(df_for_clean)
 
     def clean_card_data(): 
      
-     
+     card_df = data_extraction.DataExtractor.retrieve_pdf_data()
      card_df.reset_index(drop=True,inplace=True)
 
      card_df['card_number']= card_df['card_number'].replace('^.*\?\s*','', regex=True)
@@ -52,13 +49,6 @@ class DataCleaning:
 
 
      card_df.to_sql('dim_card_details',out_db,if_exists= 'replace')
-     print(card_df)
-
-
- 
-
-
-     
      pass
 
-test = DataCleaning.clean_card_data()
+DataCleaning.clean_card_data()
--- data_extraction.py
+++ data_extraction.py
@@ -6,18 +6,18 @@ import pandas as pd
 
 class DataExtractor:
     """extracting database detail from the link in database_utils"""
-    def __init__(self):
-       self.users = self.read_rds_table()
-       self.card_util = self.retrieve_pdf_data()
+    def __init__():
+
        pass
 
-    def read_rds_table(self):
+    def read_rds_table():
        
         read_rds_table_df = pd.read_sql_query('SELECT * FROM legacy_users',db_conn.engine)
+        
 
         return read_rds_table_df
     
-    def retrieve_pdf_data(self):
+    def retrieve_pdf_data():
         """Process to retrieve data stored in a PDF
         TABULA add on for Pyton (see top of file)
         IMPORT the read-pdf function
@@ -25,7 +25,7 @@ class DataExtractor:
         """
 
         pdf_link = 'https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf'#held as a variable so that can be changed easily at a later date.
-        card_data = read_pdf(pdf_link,pages= 'all') #pages= 'all' or only the first page is imported to dataframe
+        card_data = read_pdf(pdf_link, pages= 'all') #pages= 'all' or only the first page is imported to dataframe
         card_data_df = pd.concat(card_data)   #each page of PDF is a seperate list, so needs to be concatenated
 
         return card_data_df
@@ -36,5 +36,5 @@ class DataExtractor:
 
         pass
 
-test = DataExtractor().card_util()
+
 
